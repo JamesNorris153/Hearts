51fea9d98458756b362b79ebc168212f3b237e94
James Norris <James.Norris153@gmail.com>
2021-01-05 21:07:03 +0000

    Initial Commit

---
+++ Hearts/HeartsApp.swift
@@ -0,0 +1,20 @@
+//
+//  HeartsApp.swift
+//  Hearts
+//
+//  Created by James Norris on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HeartsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
